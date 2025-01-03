9013223145d11cc114ee37a6033ba4473885e6c4
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-03 17:41:29 +0530

    connecting views with the urls

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path("",views.start_app)
+    path("",views.start_app),
+    path("all/",views.all_todos),
+    path("post/",views.post_todo)
 ]
