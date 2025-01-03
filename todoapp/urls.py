0d3c226150b35e5575ce9dcd9eb486ca62b09d58
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-03 20:39:00 +0530

    PUT and DELETE request

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns =[
     path("",views.start_app),
     path("all/",views.all_todos),
-    path("post/",views.post_todo)
+    path("post/",views.post_todo),
+    path("update/<int:pk>/",views.update_todo),
+    path("delete/<int:pk>/",views.delete_todo),
 ]
